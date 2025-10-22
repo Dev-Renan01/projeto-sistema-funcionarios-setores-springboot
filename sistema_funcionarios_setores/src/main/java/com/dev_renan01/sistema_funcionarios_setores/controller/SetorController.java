8716dac385f4f666bf6a9c6052c062ec7084c67f
Thiago Renan <thiagorenan1108@gmail.com>
2025-10-22 00:14:34 -0300

    CRUD - Setor

--- sistema_funcionarios_setores/src/main/java/com/dev_renan01/sistema_funcionarios_setores/controller/SetorController.java
+++ sistema_funcionarios_setores/src/main/java/com/dev_renan01/sistema_funcionarios_setores/controller/SetorController.java
@@ -1,9 +1,13 @@
 package com.dev_renan01.sistema_funcionarios_setores.controller;
 
+import com.dev_renan01.sistema_funcionarios_setores.model.Setor;
 import com.dev_renan01.sistema_funcionarios_setores.repository.SetorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/setor")
@@ -13,5 +17,44 @@ public class SetorController {
     private SetorRepository setorRepository;
 
 
+    @GetMapping(value = "/listar")
+    @ResponseBody
+    public ResponseEntity<List<Setor>> listarTodos(){
+       List<Setor> setor = setorRepository.findAll();
 
+        return new ResponseEntity<List<Setor>>(setor, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/salvar")
+    @ResponseBody
+    public ResponseEntity<Setor> salvarSetor(@RequestBody Setor setor){
+        Setor salvarSetor = setorRepository.save(setor);
+
+        return new ResponseEntity<>(salvarSetor, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/atualizar")
+    @ResponseBody
+    public ResponseEntity<?> atualizarSetor(@RequestBody Setor setor){
+
+        if(setor.getId() != null){
+            Setor atualizarSetor = setorRepository.saveAndFlush(setor);
+
+            return new ResponseEntity<Setor>(atualizarSetor, HttpStatus.OK);
+        }else{
+            return new ResponseEntity<String>("Id não encontrado!", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping(value = "/deletar")
+    @ResponseBody
+    public ResponseEntity<String> deletarPorId(@RequestParam Long id){
+        if(id != null){
+            setorRepository.deleteById(id);
+
+            return new ResponseEntity<String>("Deletado com sucesso.", HttpStatus.OK);
+        }else{
+            return new ResponseEntity<String>("Id não encontrado!", HttpStatus.NOT_FOUND);
+        }
+    }
 }
